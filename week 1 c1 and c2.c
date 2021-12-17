61a541d81995c982986ef99ae194c60d04c92dbe
huukhai243 <91890454+huukhai243@users.noreply.github.com>
2021-12-18 00:03:59 +0700

    Add files via upload

---
+++ week 1 c1 and c2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+void replace_char(char *str, char c1, char c2)
+{
+if (str == NULL)
+return;
+while (*str != '\0')
+{
+if (*str == c1)
+*str = c2;
+++str;
+}
+}
+void main(){
+	
+}
+}
